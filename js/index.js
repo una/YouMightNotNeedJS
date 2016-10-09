eb53bd01687a87a00a643608d5baac270bc4ea40
Una Kravets <una.kravets@gmail.com>
2016-10-08 23:54:29 -0400

    added accordion

--- js/index.js
+++ js/index.js
@@ -108,7 +108,12 @@
   };
 
   document.addEventListener('DOMContentLoaded', function() {
-    var search, slider;
+    var handleChange, search, slider;
+    slider = document.querySelector('.version-slider');
+    (handleChange = function() {
+      return setMinVersion(slider.value);
+    })();
+    slider.addEventListener('change', handleChange);
     search = document.querySelector('input[type="search"]');
     return search.addEventListener('input', function() {
       return filter(search.value);
